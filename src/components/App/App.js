d6152c9210289d80b1fd5e150433396f4f01a4b9
Andrei Motinga <amotinga@AndreiMtingaPro.home>
2018-12-21 11:03:13 -0500

    add routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Router } from "@reach/router"
 import classNames from "classnames"
 import { withStyles } from "@material-ui/core/styles"
 import PersistentDrawer from "./PersistentDrawer"
 import Nav from "./Nav"
 import { DRAWER_WIDTH } from "config"
-
-const styles = theme => ({
-  root: {
-    display: "flex"
-  },
-  content: {
-    marginTop: theme.spacing.unit * 6,
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    marginLeft: -DRAWER_WIDTH
-  },
-  contentWithOpenDrawer: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
-  }
-})
+import Home from "components/pages/Home"
 
 class PersistentDrawerLeft extends React.Component {
   state = {
@@ -52,21 +31,9 @@ class PersistentDrawerLeft extends React.Component {
             [classes.contentWithOpenDrawer]: isDrawerOpen
           })}
         >
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-            dolor purus non enim praesent elementum facilisis leo vel. Risus at
-            ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
-            rutrum quisque non tellus. Convallis convallis tellus id interdum
-            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
-            sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-            integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-            eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-            quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-            vivamus at augue. At augue eget arcu dictum varius duis at
-            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.
-          </p>
+          <Router>
+            <Home path="/" />
+          </Router>
         </main>
       </div>
     )
@@ -76,5 +43,28 @@ class PersistentDrawerLeft extends React.Component {
 PersistentDrawerLeft.propTypes = {
   classes: PropTypes.object.isRequired
 }
+
+const styles = theme => ({
+  root: {
+    display: "flex"
+  },
+  content: {
+    marginTop: theme.spacing.unit * 6,
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: -DRAWER_WIDTH
+  },
+  contentWithOpenDrawer: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    marginLeft: 0
+  }
+})
 
 export default withStyles(styles)(PersistentDrawerLeft)
