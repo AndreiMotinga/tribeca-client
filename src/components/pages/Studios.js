194db2b15d4d2745e54a7cf6e091025eef06cd53
Andrei Motinga <amotinga@AndreiMtingaPro.home>
2018-12-21 16:48:03 -0500

    display studios

--- src/components/pages/Studios.js
+++ src/components/pages/Studios.js
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import axios from "axios"
 import { withStyles } from "@material-ui/core/styles"
 import Table from "@material-ui/core/Table"
 import TableBody from "@material-ui/core/TableBody"
@@ -19,53 +20,49 @@ const styles = theme => ({
   }
 })
 
-let id = 0
-function createData(name, calories, fat, carbs, protein) {
-  id += 1
-  return { id, name, calories, fat, carbs, protein }
-}
+class SimpleTable extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      records: []
+    }
+  }
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9)
-]
+  componentDidMount = () => {
+    axios.get("/studios").then(data => {
+      this.setState({ records: data.data })
+    })
+  }
 
-function SimpleTable(props) {
-  const { classes } = props
+  render() {
+    const { records } = this.state
+    const { classes } = this.props
 
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat (g)</TableCell>
-            <TableCell align="right">Carbs (g)</TableCell>
-            <TableCell align="right">Protein (g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map(row => {
-            return (
-              <TableRow key={row.id}>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
-              </TableRow>
-            )
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
-  )
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell padding="dense">Id</TableCell>
+              <TableCell>Title</TableCell>
+              <TableCell>Created</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {records.map(record => {
+              return (
+                <TableRow key={record.id}>
+                  <TableCell padding="dense">{record.id}</TableCell>
+                  <TableCell>{record.title}</TableCell>
+                  <TableCell>{record.created_at}</TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    )
+  }
 }
 
 SimpleTable.propTypes = {
