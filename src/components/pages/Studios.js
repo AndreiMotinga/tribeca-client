34e13c40e62325a389a40c028e599720d187bfd6
Andrei Motinga <amotinga@AndreiMtingaPro.home>
2018-12-21 16:56:33 -0500

    add header

--- src/components/pages/Studios.js
+++ src/components/pages/Studios.js
@@ -9,6 +9,14 @@ import TableHead from "@material-ui/core/TableHead"
 import TableRow from "@material-ui/core/TableRow"
 import Paper from "@material-ui/core/Paper"
 
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import IconButton from "@material-ui/core/IconButton"
+import Tooltip from "@material-ui/core/Tooltip"
+import DeleteIcon from "@material-ui/icons/Delete"
+import FilterListIcon from "@material-ui/icons/FilterList"
+import { lighten } from "@material-ui/core/styles/colorManipulator"
+
 const styles = theme => ({
   root: {
     width: "100%",
@@ -17,6 +25,18 @@ const styles = theme => ({
   },
   table: {
     minWidth: 700
+  },
+
+  title: {
+    flex: "0 0 auto"
+  },
+
+  spacer: {
+    flex: "1 1 100%"
+  },
+
+  actions: {
+    color: theme.palette.text.secondary
   }
 })
 
@@ -40,6 +60,20 @@ class SimpleTable extends React.Component {
 
     return (
       <Paper className={classes.root}>
+        <Toolbar>
+          <div className={classes.title}>
+            <Typography variant="h6" id="tableTitle">
+              Studios
+            </Typography>
+          </div>
+          <div className={classes.spacer} />
+          <div className={classes.actions}>
+            <IconButton aria-label="Filter list">
+              <FilterListIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+
         <Table className={classes.table}>
           <TableHead>
             <TableRow>
