d6ebd9fbdec86cd50d25ca818d88ddb0797c248e
Andrei Motinga <amotinga@AndreiMtingaPro.home>
2018-12-27 10:51:00 -0500

    setup form

--- src/components/pages/StudioForm.js
+++ src/components/pages/StudioForm.js
@@ -1,15 +1,34 @@
 import React from "react"
+import axios from "axios"
 
 class StudioForm extends React.Component {
+  state = {
+    studio: null
+  }
+
+  componentDidMount = () => {
+    axios.get(`/studios/${this.props.id}`).then(res => {
+      this.setState({
+        studio: res.data
+      })
+    })
+  }
+
   submit = e => {
     e.preventDefault()
     console.log("submitting form")
   }
 
   render() {
+    const studio = this.state.studio
+
+    if (!studio) {
+      return null
+    }
+
     return (
       <form onSubmit={this.submit}>
-        <h1>form here</h1>
+        <h1>{studio.title}</h1>
         <button onClick={this.submit}>Submit</button>
       </form>
     )
